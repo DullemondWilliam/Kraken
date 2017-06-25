6411f25517e4cd49caa0345c0d09e91676fc696c
Mitchell Turner <mitchellt626@gmail.com>
2017-06-25 19:02:20 -0400

    This is a commit

---
+++ Source/MyProject/Private/ProceduralGeneration.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProceduralGeneration.h"
+
+ProceduralGeneration::ProceduralGeneration()
+{
+}
+
+ProceduralGeneration::~ProceduralGeneration()
+{
+}
+int ProceduralGeneration::getRandomInteger(){
+	
+}
